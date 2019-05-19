1d9c32ac3fa1f031febf2d536e2075a3b3b7560c
Gerry Jerome <gerry.jerome@zerar.co.uk>
2019-05-19 18:59:33 +0100

    Static files and template inheritance

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -15,3 +15,13 @@ def index(request):
             'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
         }
     )
+
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
+        }
+    )
--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -27,4 +27,6 @@ import HelloDjangoApp.views
 urlpatterns = [
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
+
 ]
